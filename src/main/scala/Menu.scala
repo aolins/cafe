fe31fd5811d85d942191a84052bd230840a3b524
Andrejs Olins <andrejs.olins@gmail.com>
2018-11-07 23:44:46 +0200

    step 2; change item api

--- src/main/scala/Menu.scala
+++ src/main/scala/Menu.scala
@@ -2,11 +2,12 @@ object Menu {
 
   class Item(
     val price: BigDecimal,
-    val name:String
+    val name:String,
+    val isDrink:Boolean = false
   )
 
-  object ColaCold extends Item(0.5, "Cola - Cold")
-  object CoffeeHot extends Item(1, "Coffee - Hot")
+  object ColaCold extends Item(0.5, "Cola - Cold", true)
+  object CoffeeHot extends Item(1, "Coffee - Hot", true)
   object CheeseSandwichCold extends Item(2, "Cheese Sandwich - Cold")
   object SteakSandwichHot extends Item(4.5, "Steak Sandwich - Hot")
 
