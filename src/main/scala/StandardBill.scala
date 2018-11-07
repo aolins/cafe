27f2bde10d111c03ae70f7ee8832450e8639d926
Andrejs Olins <andrejs.olins@gmail.com>
2018-11-07 23:36:03 +0200

    step 1 green test

--- src/main/scala/StandardBill.scala
+++ src/main/scala/StandardBill.scala
@@ -2,5 +2,7 @@ import Menu.Item
 
 object StandardBill {
 
-  def calculateTotal(list:List[Item]):BigDecimal = ???
+  def calculateTotal(list:List[Item]):BigDecimal = {
+    list.foldLeft(BigDecimal(0))((acc, item) => acc + item.price)
+  }
 }
